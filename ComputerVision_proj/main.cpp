afb12ca8cf5b2e3909142fc109ad16a8ffea2bfb
Obvios <palmae95@gmail.com>
2019-08-27 14:34:26 -0700

    created main for computer vision project

--- ComputerVision_proj/main.cpp
+++ ComputerVision_proj/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
+using namespace cv;
 int main(int argc, char * argv[]) {
-	std::cout << "hello\n";
+	std::cout << CV_VERSION ;
 	std::cin.get();
 
 	return 0;
